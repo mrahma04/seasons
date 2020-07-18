dc73c57459a6923dbf0c14b19f12ea5d3edbaf8a
EC2 Default User <ec2-user@ip-172-31-59-158.ec2.internal>
2020-07-18 23:20:17 +0000

    added SeasonDisplay and styles

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './styles.css';
 
-const SeasonDisplay = () => {
+const getSeason = (month, lat) => {
+    if (month > 2 && month < 10) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr it's cold",
+        iconName: 'snowflake'
+    }
+};
+
+const SeasonDisplay = (props) => {
+
+    const season = getSeason(new Date().getMonth(), props.lat);
+    const { text, iconName } = seasonConfig[season];
+
     return(
-        <div>SeaonDisplay</div>
-    );
+        <div className="season-display {season}">
+            <i className={`icon-left massive ${iconName} icon`}></i>
+            <div className={season}>{text}</div>
+            <i className={`icon-right massive ${iconName} icon`}></i>
+         </div>
+    )
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
+
+    state = { lat: null, errorMessage: '' };  
+    
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        )
+    } 
+
     render() {
-        return(
-            <SeasonDisplay />
-        );
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        } else {
+            return <div>Loading...</div>
+        }
     }
 };
 
