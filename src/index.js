8f1d828e149e3d83bdd74e1aed98b49cfa3d9e0a
EC2 Default User <ec2-user@ip-172-31-59-158.ec2.internal>
2020-07-18 16:14:06 +0000

    changed index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello form AWS Amplify</div>;
+    return <div>Hello form AWS Amplify!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
