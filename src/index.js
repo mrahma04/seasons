7f0a7969e28b09b7927a0e88cc4caca16e02acef
EC2 Default User <ec2-user@ip-172-31-59-158.ec2.internal>
2020-07-18 15:43:48 +0000

    hola

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello</div>;
+    return <div>Hola! My name is Dora!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
